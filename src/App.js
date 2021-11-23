6191ecfcb1b11e6fcab569a7ede51666bad23a34
Ishan8827 <ishanpatel2468@gmail.com>
2021-11-24 02:39:30 +0530

    covidapp

--- src/App.js
+++ src/App.js
@@ -1,24 +1,52 @@
+import React,{useState,useEffect} from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+
 function App() {
+  const[data,setdata]=useState([])
+  const getdata= async ()=>{
+  const response= await fetch("https://data.covid19india.org/data.json");
+  const resp=await response.json();
+  console.log(resp.statewise[0]);
+  setdata(resp.statewise[0]);
+  }
+  useEffect(()=>{getdata()},[]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="bg">
+    <center><div className="one">
+     <h2>Ishan covid tracker(India)</h2>
+     </div>
+     </center>
+      <br/>
+      <center><div className="two">
+     <h3>Active cases={data.active }</h3>
+     </div>
+     </center>
+      <br/>
+      <center><div className="three">
+     <h3>Confirmed cases={data.confirmed }</h3>
+     </div>
+     </center>
+      <br/>
+      <center><div className="four">
+     <h3>deaths={data.deaths }</h3>
+     </div>
+     </center>
+      <br/>
+      <center><div className="five">
+     <h3>Recovered={data.recovered }</h3>
+       </div>
+     </center>
+     <br/>
+     <center><div className="five">
+     <h3>lastupdatedtime={data.lastupdatedtime }</h3>
+     </div>
+     </center>
+     </div>
+    
+     
+  
   );
 }
 
